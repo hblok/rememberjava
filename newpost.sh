3dbd7b8ccda6ad707a3d8f0732523f9b351d8d4e
hblok <gitmail@hblok.net>
2016-10-10 18:17:27 +0200

    newpost.sh

---
+++ newpost.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+day=`date +"%Y-%m-%d"`
+time=`date +"%Y-%m-%d %H:%M:%S %z"`
+file="_posts/${day}-${1}.md"
+
+cat > $file << EOF
+layout: post
+title:  
+date:   ${time}
+categories: 
+---
+
+
+{% highlight java %}
+{% include src/.java %}
+{% endhighlight %}
+
+EOF
+
