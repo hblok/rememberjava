cea3366fd3674ea31fa9d267d2ee1ccf4509c13e
hblok@tp <hblok@tp>
2017-01-03 17:57:43 +0100

    Started MidiApiTest

---
+++ _includes/src/com/rememberjava/midi/MidiApiTest.java
@@ -0,0 +1,64 @@
+package com.rememberjava.midi;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiDevice.Info;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Synthesizer;
+import javax.sound.midi.Transmitter;
+
+import org.junit.Test;
+
+/**
+ * See also: https://docs.oracle.com/javase/tutorial/sound/overview-MIDI.html
+ */
+
+public class MidiApiTest {
+
+  @Test
+  public void testGetMidiDeviceInfo() {
+    Info[] infos = MidiSystem.getMidiDeviceInfo();
+    for (Info info : infos) {
+      System.out.println("Info: " + info.getName() + ": " + info.getVendor() + ", "
+          + info.getVersion() + ", " + info.getDescription());
+
+    }
+  }
+
+  @Test
+  public void testGetSequencer() throws MidiUnavailableException {
+    Sequencer sequencer = MidiSystem.getSequencer();
+    Transmitter transmitter = sequencer.getTransmitter();
+
+    System.out.println("Sequencer: " + sequencer.getDeviceInfo());
+    System.out.println("Transmitter: " + transmitter);
+  }
+
+  @Test
+  public void testGetSynthesizer() throws MidiUnavailableException {
+    Synthesizer synthesizer = MidiSystem.getSynthesizer();
+    Receiver receiver = synthesizer.getReceiver();
+
+    System.out.println("Synthesizer: " + synthesizer.getDeviceInfo());
+    System.out.println("Receiver: " + receiver);
+  }
+
+  @Test
+  public void testPlayNote() throws InvalidMidiDataException, MidiUnavailableException {
+    ShortMessage msg = new ShortMessage();
+
+    // 60 = middle C, C4 (or C3 or C5)
+    // https://en.wikipedia.org/wiki/Scientific_pitch_notation#Similar_systems
+    int channel = 0;
+    int note = 60;
+    int velocity = 127; // velocity (i.e. volume); 127 = high
+    msg.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+
+    long timeStamp = -1;
+    Receiver receiver = MidiSystem.getReceiver();
+    receiver.send(msg, timeStamp);
+  }
+}
