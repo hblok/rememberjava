20d7a42585818c7b90675166dd378ee146242d4a
hblok@tp <hblok@tp>
2017-03-14 17:56:30 +0100

    filename

--- newpost.sh
+++ newpost.sh
@@ -4,6 +4,12 @@ day=`date +"%Y-%m-%d"`
 time=`date +"%Y-%m-%d %H:%M:%S %z"`
 file="_posts/${day}-${1}.md"
 
+java=$2
+java=`echo $java | sed "s@_includes/@@"`
+if [[ "${java}" != *"src/com/rememberjava/"* ]]; then
+  java="src/com/rememberjava/${java}"
+fi
+
 cat > $file << EOF
 ---
 published: false
@@ -16,11 +22,11 @@ tags:
 ---
 
 {% highlight java %}
-{% include includemethod filename='src/com/rememberjava/.java' method='()' before=0  after=0 %}
+{% include includemethod filename='${java}' method='()' before=0  after=0 %}
 {% endhighlight %}
 
 
-{% include javafile filename='src/com/rememberjava/.java' %}
+{% include javafile filename='${java}' %}
 
 EOF
 
